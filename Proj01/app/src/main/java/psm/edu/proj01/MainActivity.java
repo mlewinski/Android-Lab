7173c0c029ce530bee00dc90b767b0d2e0bd9911
Marek Lewiński <lewinski.marek@outlook.com>
2018-03-16 21:17:17 +0100

    Finished Proj01

--- Proj01/app/src/main/java/psm/edu/proj01/MainActivity.java
+++ Proj01/app/src/main/java/psm/edu/proj01/MainActivity.java
@@ -1,13 +1,49 @@
 package psm.edu.proj01;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements android.view.View.OnClickListener{
+
+    private Button buttonPK;
+    private Button buttonDial;
+    private Button buttonContacts;
+    private Button buttonContactInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonPK = (Button) findViewById(R.id.buttonPK);
+        buttonDial = (Button) findViewById(R.id.buttonDial);
+        buttonContacts = (Button) findViewById(R.id.buttonContacts);
+        buttonContacts = (Button) findViewById(R.id.buttonContactInfo);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        switch(view.getId()){
+            case R.id.buttonPK:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.pk.edu.pl"));
+                startActivity(intent);
+                break;
+            case R.id.buttonDial:
+                intent = new Intent(Intent.ACTION_DIAL, Uri.parse(""));
+                startActivity(intent);
+                break;
+            case R.id.buttonContacts:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("content://contacts/people"));
+                startActivity(intent);
+                break;
+            case R.id.buttonContactInfo:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("content://contacts/people/1"));
+                startActivity(intent);
+                break;
+        }
     }
 }
